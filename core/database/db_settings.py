235a25a5fc719d07983db9e96f3d1bb23208825a
DinarDi <dinktmt@gmail.com>
2024-02-14 17:25:30 +0500

    Create database settings

---
+++ core/database/db_settings.py
@@ -0,0 +1,34 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from core.config import settings
+
+
+class DatabaseSettings:
+    """
+    Settings for database
+    """
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+
+        self.async_session = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+            class_=AsyncSession,
+        )
+
+    async def create_async_session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.async_session as session:
+            yield session
+
+
+db_settings = DatabaseSettings(
+    url=settings.DB_URL,
+    echo=settings.db_echo,
+)
