bdab18e475a7216bfb6b36cc1fae106b6004495a
DinarDi <dinktmt@gmail.com>
2024-02-15 17:14:16 +0500

    Create schemas for account

---
+++ api_v1/account/schemas.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict
+
+
+class AccountBase(BaseModel):
+    pass
+
+
+class AccountRead(AccountBase):
+    id: int
+    balance: Decimal
+
+    model_config = ConfigDict(from_attributes=True)
