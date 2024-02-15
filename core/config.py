e1eefa41e25f5f282731b707c7491ec351eaa9b5
DinarDi <dinktmt@gmail.com>
2024-02-16 00:04:08 +0500

    Create settings for JWT

--- core/config.py
+++ core/config.py
@@ -1,9 +1,21 @@
+from datetime import timedelta
 from pathlib import Path
 
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 BASE_DIR = Path(__file__).parent.parent
+
+
+class AuthJWT(BaseModel):
+    """
+    Settings for JWT
+    """
+    private_key_path = BASE_DIR / 'certs' / 'jwt-private.pem'
+    public_key_path = BASE_DIR / 'certs' / 'jwt-public.pem'
+    algorithm: str = 'RS256'
+    access_token_exp: timedelta = timedelta(days=1)
 
 
 class Settings(BaseSettings):
@@ -13,6 +25,8 @@ class Settings(BaseSettings):
 
     DB_URL: str
     db_echo: bool = True
+    auth_jwt: AuthJWT = AuthJWT()
+
     model_config = SettingsConfigDict(env_file='.env', extra='allow')
 
 
