9c586a3be4051dc4d7cea003d9903c1abacbe0be
DinarDi <dinktmt@gmail.com>
2024-02-16 00:04:37 +0500

    Create functions for encode, decode jwt

--- api_v1/auth/utils.py
+++ api_v1/auth/utils.py
@@ -1,4 +1,9 @@
+from datetime import timedelta, datetime, timezone
+
 import bcrypt
+import jwt
+
+from core.config import settings
 
 
 def hash_password(
@@ -25,3 +30,42 @@ def validate_password(
         password=password.encode(),
         hashed_password=hashed_password,
     )
+
+
+def encode_jwt(
+        payload: dict,
+        private_key: str = settings.auth_jwt.private_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+        expire_timedelta: timedelta = settings.auth_jwt.access_token_exp,
+):
+    """
+    Function for encode JWT
+    """
+    to_encode = payload.copy()
+    expire = datetime.now(timezone.utc) + expire_timedelta
+    to_encode.update(
+        exp=expire,
+    )
+
+    encoded = jwt.encode(
+        payload=to_encode,
+        key=private_key,
+        algorithm=algorithm,
+    )
+    return encoded
+
+
+def decode_jwt(
+        token: str | bytes,
+        public_key: str = settings.auth_jwt.public_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+):
+    """
+    Function for decode JWT
+    """
+    decoded = jwt.decode(
+        jwt=token,
+        key=public_key,
+        algorithms=[algorithm],
+    )
+    return decoded
