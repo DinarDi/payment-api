4f2d362ea75ecadbd175abff2ea0426a987c1afb
DinarDi <dinktmt@gmail.com>
2024-02-15 16:20:35 +0500

    Update imports

--- core/database/models/user.py
+++ core/database/models/user.py
@@ -1,14 +1,20 @@
-from typing import Optional
+from typing import TYPE_CHECKING
 
 from sqlalchemy import String, LargeBinary
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+
+if TYPE_CHECKING:
+    from .account import Account
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str]
-    first_name: Mapped[Optional[str]] = mapped_column(String(50))
-    last_name: Mapped[Optional[str]] = mapped_column(String(50))
+    first_name: Mapped[str | None] = mapped_column(String(50))
+    last_name: Mapped[str | None] = mapped_column(String(50))
     password: Mapped[bytes] = mapped_column(LargeBinary())
+
+    account: Mapped['Account'] = relationship(back_populates='user')
