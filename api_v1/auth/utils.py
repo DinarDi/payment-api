31094b744b519072804fd685833f4e1384dc882f
DinarDi <dinktmt@gmail.com>
2024-02-16 00:03:25 +0500

    Create function for validate password

--- api_v1/auth/utils.py
+++ api_v1/auth/utils.py
@@ -9,3 +9,19 @@ def hash_password(
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def validate_password(
+        password: str,
+        hashed_password: bytes,
+) -> bool:
+    """
+    Function to check password
+    :param password: str
+    :param hashed_password: bytes
+    :return: True | False
+    """
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
