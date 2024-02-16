dcd8700a0162adb5a192b7c9dd3cabaae4b8e871
DinarDi <dinktmt@gmail.com>
2024-02-16 16:14:51 +0500

    Create user login

--- api_v1/auth/views.py
+++ api_v1/auth/views.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users import crud
-from api_v1.users.schemas import UserCreate, UserRead
+from api_v1.users.schemas import UserCreate, UserRead, UserLogin
+from . import utils
+from .schemas import Token
+from core.database.models import User
 from core.database import db_settings
 
 router = APIRouter(
@@ -18,4 +21,41 @@ async def create_user(
     return await crud.create_user(
         session=session,
         payload=payload
+    )
+
+
+@router.post('/login', response_model=Token)
+async def login_user(
+        payload: UserLogin,
+        session: AsyncSession = Depends(db_settings.create_async_session),
+):
+    error = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='invalid username or password'
+    )
+    # check user exists
+    user: User | None = await crud.get_user_by_username(
+        session=session,
+        username=payload.username,
+    )
+    if not user:
+        raise error
+
+    # check password match
+    if not utils.validate_password(
+            password=payload.password,
+            hashed_password=user.password,
+    ):
+        raise error
+
+    # create token
+    jwt_payload = {
+        'sub': user.username,
+        'email': user.email
+    }
+    access_token = utils.encode_jwt(jwt_payload)
+
+    return Token(
+        access_token=access_token,
+        token_type='Bearer',
     )
