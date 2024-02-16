c748d7b94f2b92984245c859cfc12aac30b1be48
DinarDi <dinktmt@gmail.com>
2024-02-16 16:49:51 +0500

    Create enum for token mode, add field for refresh token

--- api_v1/auth/schemas.py
+++ api_v1/auth/schemas.py
@@ -1,7 +1,14 @@
+from enum import Enum
+
 from pydantic import BaseModel
 
 
+class TokenModeEnum(str, Enum):
+    access = 'access_token'
+    refresh = 'refresh_token'
+
+
 class Token(BaseModel):
-    access_token: str
     token_type: str
-    
+    access_token: str
+    refresh_token: str
