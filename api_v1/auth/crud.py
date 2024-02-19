bfd15b84299369fca2bebd30552c9eba56824db3
DinarDi <dinktmt@gmail.com>
2024-02-19 15:43:52 +0500

    Create function to get token by user id

--- api_v1/auth/crud.py
+++ api_v1/auth/crud.py
@@ -5,12 +5,22 @@ from api_v1.auth.schemas import CreateRefreshToken
 from core.database.models import Token
 
 
+async def get_token_by_user_id(
+        session: AsyncSession,
+        user_id: int,
+) -> Token:
+    stmt = select(Token).where(Token.user_id == user_id)
+    result: Result = await session.execute(stmt)
+    token = result.scalar_one_or_none()
+    return token
+
+
 async def create_refresh_token(
         session: AsyncSession,
         payload: CreateRefreshToken,
 ) -> Token:
     # Get exists token
-    payload = payload.model_dump(exclude_unset=True)
+    payload = payload.model_dump()
     stmt = select(Token).where(Token.user_id == payload.get('user_id'))
     result: Result = await session.execute(stmt)
     token = result.scalar_one_or_none()
