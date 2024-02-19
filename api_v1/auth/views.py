124112e9eb00e65df26419a532a8f1050f597bc7
DinarDi <dinktmt@gmail.com>
2024-02-19 15:44:12 +0500

    Create function to generate new tokens

--- api_v1/auth/views.py
+++ api_v1/auth/views.py
@@ -1,17 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from jwt.exceptions import PyJWTError
 
 from api_v1.users import crud as users_crud
 from api_v1.auth import crud as auth_crud
 from api_v1.users.schemas import UserCreate, UserRead, UserLogin
 from . import utils
 from .schemas import TokenRead, TokenModeEnum, CreateRefreshToken
-from core.database.models import User
+from core.database.models import User, Token
 from core.database import db_settings
 
 router = APIRouter(
     tags=['Auth'],
 )
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/refresh_token')
 
 
 @router.post('/register', response_model=UserRead)
@@ -73,3 +78,59 @@ async def login_user(
         access_token=access_token,
         refresh_token=refresh_token,
     )
+
+
+async def get_new_token(
+        token: str = Depends(oauth_scheme),
+        session: AsyncSession = Depends(db_settings.create_async_session),
+):
+    error = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail={'invalid credentials'},
+    )
+    try:
+        payload = utils.decode_jwt(token=token)
+        if 'sub' not in payload and 'mode' not in payload:
+            raise error
+        # check token mode
+        if payload['mode'] != TokenModeEnum.refresh:
+            raise error
+        # get user
+        user = await users_crud.get_user_by_username(username=payload['sub'], session=session)
+        if not user:
+            raise error
+        # get refresh token from database
+        db_token: Token = await auth_crud.get_token_by_user_id(user_id=user.id, session=session)
+        if token != db_token.refresh_token:
+            raise error
+        # generate new refresh token
+        jwt_payload = {
+            'sub': user.username,
+            'email': user.email,
+        }
+        new_refresh_token = utils.encode_jwt(
+            payload=jwt_payload,
+            token_mode=TokenModeEnum.refresh,
+        )
+        # save new refresh token in database
+        setattr(db_token, 'refresh_token', new_refresh_token)
+        await session.commit()
+        # generate new access token
+        new_access_token = utils.encode_jwt(
+            payload=jwt_payload,
+            token_mode=TokenModeEnum.access,
+        )
+    except PyJWTError:
+        raise error
+
+    return TokenRead(
+        token_type='Bearer',
+        access_token=new_access_token,
+        refresh_token=new_refresh_token,
+    )
+
+
+@router.post('/refresh_token')
+async def refresh_tokens(token: TokenRead = Depends(get_new_token)):
+
+    return token
