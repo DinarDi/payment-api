d0cea97c4b97182c94bc7a9ba051305382deb184
DinarDi <dinktmt@gmail.com>
2024-02-20 20:11:58 +0500

    Create function to delete token from database

--- api_v1/auth/crud.py
+++ api_v1/auth/crud.py
@@ -28,3 +28,16 @@ async def create_refresh_token(
     setattr(token, 'refresh_token', payload.get('refresh_token'))
     await session.commit()
     return token
+
+
+async def delete_refresh_token(
+        session: AsyncSession,
+        user_id: int,
+
+):
+    stmt = select(Token).where(Token.user_id == user_id)
+    result: Result = await session.execute(stmt)
+    token = result.scalar_one_or_none()
+    setattr(token, 'refresh_token', None)
+    await session.commit()
+    return None
