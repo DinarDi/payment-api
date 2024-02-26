54c731301ebf23e5e12590eae772f8eb95381795
DinarDi <dinktmt@gmail.com>
2024-02-26 20:55:46 +0500

    Create router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .auth.views import router as auth_router
+from .balance_change.views import router as balance_router
 
 router = APIRouter()
 router.include_router(router=auth_router, prefix='/auth')
+router.include_router(router=balance_router, prefix='/balance')
