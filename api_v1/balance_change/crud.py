ceb0da1134c906ba411bff4e151a51af25bfe5cf
DinarDi <dinktmt@gmail.com>
2024-02-27 22:20:28 +0500

    Crud for payment acceptance

--- api_v1/balance_change/crud.py
+++ api_v1/balance_change/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database.models import BalanceChange
@@ -17,3 +18,32 @@ async def create_change(
     await session.commit()
     await session.refresh(change)
     return change
+
+
+async def get_change_by_id(
+        session: AsyncSession,
+        change_id: int,
+):
+    stmt = select(BalanceChange).where(BalanceChange.id == change_id)
+    result: Result = await session.execute(stmt)
+    change = result.scalar_one()
+    return change
+
+
+async def update_change_is_accepted(
+        session: AsyncSession,
+        change_id: int
+):
+    change: BalanceChange = await get_change_by_id(session=session, change_id=change_id)
+    change.is_accepted = True
+    await session.commit()
+    await session.refresh(change)
+    return change
+
+
+async def delete_change(
+        session: AsyncSession,
+        change_id: int,
+):
+    change: BalanceChange = await get_change_by_id(session=session, change_id=change_id)
+    await session.delete(change)
