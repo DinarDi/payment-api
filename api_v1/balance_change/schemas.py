aef09d87addb0b032a15e349f567c94e9122cbe2
DinarDi <dinktmt@gmail.com>
2024-02-27 22:19:48 +0500

    Create schemas for payment acceptance

--- api_v1/balance_change/schemas.py
+++ api_v1/balance_change/schemas.py
@@ -27,4 +27,30 @@ class PaymentCreate(PaymentBase):
 
     @computed_field
     def value_with_commission(self) -> Decimal:
-        return self.value * (1 / (1 - self.commission / 100))
+        return round(self.value * (1 / (1 - self.commission / 100)), 2)
+
+
+class PaymentAcceptanceBase(BaseModel):
+    pass
+
+
+class PaymentAcceptanceMetadata(PaymentAcceptanceBase):
+    change_id: str
+    user_account_id: str
+
+
+class PaymentAcceptanceIncome(PaymentAcceptanceBase):
+    value: str
+    currency: str
+
+
+class PaymentAcceptanceObject(PaymentAcceptanceBase):
+    status: str
+    metadata: PaymentAcceptanceMetadata
+    income_amount: PaymentAcceptanceIncome
+
+
+class PaymentAcceptanceResponse(PaymentAcceptanceBase):
+    type: str
+    event: str
+    object: PaymentAcceptanceObject
